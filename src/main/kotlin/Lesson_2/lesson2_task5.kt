48ec18992a1a752659a2d93b5159a1db6cbd7257
KutuevTimur <mr.kreek68@gmail.com>
2025-11-07 19:45:44 +0300

    KS-2-5 изменил решение 5-й задачи 2-го урока

--- src/main/kotlin/Lesson_2/lesson2_task5.kt
+++ src/main/kotlin/Lesson_2/lesson2_task5.kt
@@ -3,15 +3,14 @@ package org.example.Lesson_2
 import kotlin.math.pow
 
 fun main() {
-
     val initialDeposit = 70_000.0
-    val interestRate = 16.7 / 100
+    val rate = 16.7
+
+    val interestRate = rate / 100
     val years = 20
 
     // A = deposit × (1 + percent)^years - формула сложных процентов
-    val finalAmount = initialDeposit * (1 + interestRate).pow(years.toDouble())
+    val finalAmount = initialDeposit * (1 + interestRate).pow(years)
 
     println("%.3f".format(finalAmount))
-
-
 }
