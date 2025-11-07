2f15d4309ad3fc5bdcaa95c9c8d594e9db51baa2
KutuevTimur <mr.kreek68@gmail.com>
2025-11-07 16:24:11 +0300

    KS-3-1 решение 1-й задачи 3-го урока

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val greeting = "Good"
+    val nameOfUser = "Timur"
+    var timeOfDay = "afternoon"
+    val punctuation = "!"
+
+    println("$greeting $timeOfDay, $nameOfUser$punctuation")
+
+    timeOfDay = "evening"
+    println("$greeting $timeOfDay, $nameOfUser$punctuation")
+}
