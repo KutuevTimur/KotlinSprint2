966e26048875b25ba80d85371ebd2dcaa8ecbe0f
KutuevTimur <mr.kreek68@gmail.com>
2025-11-07 18:08:02 +0300

    KS-3-2 решение 2-й задачи 3-го урока

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+    var surname = "Андреева"
+    var age = 20
+
+    println("$surname $firstName $middleName, $age")
+
+    age = 22
+    surname = "Сидорова"
+
+    println("$surname $firstName $middleName, $age")
+
+
+}
